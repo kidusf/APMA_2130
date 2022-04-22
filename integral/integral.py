7043cd1803877d7ed79c899321ed210b1e3f42fd
unknown <kidusfasil@gmail.com>
2022-04-21 22:34:35 -0400

    modified script to perform convolution

--- integral/integral.py
+++ integral/integral.py
@@ -1,13 +1,25 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 t=np.linspace(start=0, stop=2, num=21)
 h=np.array([0, 0.0854, 0.1425, 0.1779, 0.1927, 0.1921, 0.1801, 0.1604, 0.1361, 
 0.11, 0.084, 0.0597, 0.0381, 0.0198, 0.005, -0.0062, -0.0141, -0.0191, -0.0217, -0.0223, -0.0214])
-count=0
-sum=0
-for elements in t:
-    sum=sum+(h[count]*np.cos(2*elements))*0.1
-    count+=1
-print(sum)
+y=[]
+for times in range(len(t)):
+    count=0
+    sum=0
+    for elements in range(times):
+        sum=sum+(h[count]*np.cos(t[times]-2*t[elements]))*0.1
+        count+=1
+        print(t[times])
+    #print(t[times], sum)
+    y.append(sum)
+plt.plot(t, np.array(y))
+plt.title("Output of Convolution Integral")
+plt.xlabel("t")
+plt.ylabel("y(t)")
+plt.show()
+      
+
+
     
